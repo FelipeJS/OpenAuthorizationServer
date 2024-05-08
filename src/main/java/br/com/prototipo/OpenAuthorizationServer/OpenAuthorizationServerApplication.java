edb8795f88c2c0a2e1b51ffcfc69fa38ff608115
Felipe Ferreira Campos <felipe.campos@softplan.com.br>
2024-05-08 17:41:22 -0300

    Agenda 1: criação dos filtros padroes e registro de um client em memoria

---
+++ src/main/java/br/com/prototipo/OpenAuthorizationServer/OpenAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.prototipo.OpenAuthorizationServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAuthorizationServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OpenAuthorizationServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/prototipo/OpenAuthorizationServer/OpenAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prototipo.OpenAuthorizationServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
